4810bf819271c6aa58641ae1866910d898d2bc31
JWalters <joshwalters34@gmail.com>
2021-06-02 22:23:49 -0400

    update tablebody

--- README.md
+++ README.md
@@ -1,70 +1,32 @@
-# Getting Started with Create React App
+# Employee Directory
+This applicatiion allows a user to view imported employee information and will allow data to be sorted and searched upon. 
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Motivation
+My motivation for this project was to put my knowledge of React to use.  I created a site that allows users to view employee data and manipulate the data as they need.   
 
-## Available Scripts
+### Repository
 
-In the project directory, you can run:
+View my [GitHub_Repository](https://github.com/joshwalters34/React-Employee-Directory)
 
-### `npm start`
+### Website
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Visit my [Website](https://joshwalters34.github.io/React-Employee-Directory/)
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+### Images
+![Screenshot](./public/employee-directory-screenshot.png)
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+### Technologies 
+- React
+- Javascript
+- Axios
+- API
+- lodash
+- React-dom
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Installation
+To install dependencies for this application, run the following commands
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+        npm init -y
+        npm i
--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,124 +1,112 @@
 /* eslint-disable no-dupe-class-members */
 import axios from "axios";
 import React, { useState, Component } from "react";
-import TableDisplay from "./TableData"
+import TableDisplay from "./TableData";
 import API from "../utils/API";
 import SearchForm from "./SearchForm";
-import orderBy from "lodash/orderBy"
+import orderBy from "lodash/orderBy";
 
 const updateSort = {
   asc: "desc",
-  desc: "asc"
+  desc: "asc",
 };
- 
 
-  
-
-class TableBody extends Component {
-  
+export default class TableBody extends Component {
   state = {
     results: [{}],
     columnToSort: "",
-    sortDirection: ""
-     };
+    sortDirection: "",
+  };
 
- 
-
-  componentDidMount () {
-    axios.get('https://randomuser.me/api/?results=20&nat=us').then(res =>{
-      var final = res.data.results.map(({name, picture, email, phone, dob}) => ({
-        name: name.first + name.last,
-        picture: picture.medium,
-        email,
-        phone,
-        dob: dob.date
-      }))
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?results=20&nat=us").then((res) => {
+      var final = res.data.results.map(
+        ({ name, picture, email, phone, dob }) => ({
+          name: name.first + name.last,
+          picture: picture.medium,
+          email,
+          phone,
+          dob: dob.date,
+        })
+      );
       console.log(final);
       this.setState({
         results: final,
-        filteredResults: res.data.results
-      })
-    })
+        filteredResults: res.data.results,
+      });
+    });
   }
 
-  
-  
+  //  dataSearch = () => {
+  //     const [searchTerm, setSearchTerm] = useState("")
+  //     // eslint-disable-next-line no-lone-blocks
+  //     {results.filter((val) => {
+  //       if (searchTerm == "") {
+  //         return val
+  //       } else if (val.name.toLowerCase().includes(searchTerm.lowercase())) {
+  //         return val
+  //       }
+  //       }
+  //       )}
 
-//  dataSearch = () => {
-//     const [searchTerm, setSearchTerm] = useState("")
-//     // eslint-disable-next-line no-lone-blocks
-//     {results.filter((val) => {
-//       if (searchTerm == "") {
-//         return val
-//       } else if (val.name.toLowerCase().includes(searchTerm.lowercase())) {
-//         return val
-//       }
-//       }
-//       )}
+  //   }
 
-//   }
+  //   handleInputChange = event => {
+  //       const value = event.target.value;
+  //       const name = event.target.name;
+  //       this.setState({
+  //         [name]: value
+  //       });
+  //     };
 
-//   handleInputChange = event => {
-//       const value = event.target.value;
-//       const name = event.target.name;
-//       this.setState({
-//         [name]: value
-//       });
-//     };
-  
-//     // When the form is submitted, search the OMDB API for the value of `this.state.search`
-//     handleFormSubmit = event => {
-//       event.preventDefault();
-//       this.dataSearch(this.state.search);
-//     };
+  //     // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  //     handleFormSubmit = event => {
+  //       event.preventDefault();
+  //       this.dataSearch(this.state.search);
+  //     };
 
   sortData = (columnName) => {
-    this.setState(state => ({
+    this.setState((state) => ({
       columnToSort: columnName,
-      sortDirection: state.columnToSort === columnName ? updateSort[state.sortDirection] : "asc"
-    }))
-  }
+      sortDirection:
+        state.columnToSort === columnName
+          ? updateSort[state.sortDirection]
+          : "asc",
+    }));
+  };
 
   render() {
-
-  
     return (
       <div>
-      <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Employee Directory</h1>
-    <p class="lead">Click on each header to sort the column or use the search box to narrow your results.</p>
-  </div>
-</div>
-<SearchForm
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Employee Directory</h1>
+            <p class="lead">
+              Click on each header to sort the column or use the search box to
+              narrow your results.
+            </p>
+          </div>
+        </div>
+        <SearchForm
           search={this.state.search}
           handleFormSubmit={this.handleFormSubmit}
           // handleInputChange={this.handleInputChange}
         />
 
-      <table class="table">
-     
- 
-  <tbody>
-    <tr>
-    
-    hea
-    <TableDisplay 
-    // sortData={this.sortData}
-    // data={orderBy(this.state.results, this.state.columnToSort, this.state.sortDirection)}
+        <table class="table">
+          <tbody>
+            <tr>
+              <TableDisplay
+                // sortData={this.sortData}
+                // data={orderBy(this.state.results, this.state.columnToSort, this.state.sortDirection)}
 
-    results = {this.state.results} />
-    </tr>
-  
-  </tbody>
-</table>
-</div>
-
-    
+                results={this.state.results}
+              />
+            </tr>
+          </tbody>
+        </table>
+      </div>
     );
-  
-  
-}
+  }
 }
 
-export default TableBody;
